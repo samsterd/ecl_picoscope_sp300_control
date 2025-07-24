93b2b27547dc68888070a25aaf130e63ac8da932
samsterd <s.h.amsterdam92@gmail.com>
2025-07-24 14:26:17 -0400

    data.py: copied over useful code from pickleJar and sqliteUtils to speed up analysis

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-# functions for saving / loading data
+# functions for saving / loading / maniulating data
 
 # things we will want to save:
 #       all input parameters
@@ -17,6 +17,9 @@ import io
 import time
 import os
 import copy
+import pickle
+from tqdm import tqdm
+import json
 
 # Adapted from github.com/samsterd/ultrasonicTesting/database.py
 # Class for creating/saving into SQlite Database during experiments
@@ -145,9 +148,9 @@ class Database:
             detector1 array,
             potentiostatOut array,
             potentiostatTrigger array,
+            potentiostatCurrent array,
             time array,
             awg array,
-            awgBuffer array,
             awgTime array,'''
 
         # next add a column for each parameter in the input parameters dict
@@ -208,7 +211,7 @@ class Database:
         if newRow:
             query = 'INSERT INTO ' + table + ' (' + keyString + ') VALUES ' + qMarks + ';'
         else:
-            query = 'UPDATE ' + table + ' SET ' + keyString + ' = ' + qMarks + ' WHERE ' + keyCol + ' = ?'
+            query = 'UPDATE ' + table + ' SET (' + keyString + ') = ' + qMarks + ' WHERE ' + keyCol + ' = ?'
             safeVals.append(keyVal)
 
         return query, safeVals
@@ -281,3 +284,224 @@ class Database:
             None
         """
         self.connection.close()
+
+'''
+Adapted from ultrasonicTesting/pickleJar.py and sqliteUtils.py
+Code for interfacing with data as a pickled dict
+Format of data dict: Top level keys are integers or the string 'fileName'
+Integers are the experimentNumber data points and their values are dicts whose keys are the data and parameters of each 
+experiment in a file ( = the columns of the Sqlite database)
+'fileName' value is a string that is the path to the pickle file
+Example:
+ { 0 : {'timeCollected' : ..., 'detector0' : [...], ...},
+   1 : {...},
+   'fileName' : 'C://path//to//data.pickle'
+   }
+   
+This copying and modifying is somewhat inefficient, but I'd rather not reinvent the wheel at this point
+'''
+
+# Open a connection to the database specified in filepath and initialize a cursor
+# Returns the database connection object and the initialized cursor
+def openDB(fileName):
+
+    connection = sqlite3.connect(fileName, detect_types=sqlite3.PARSE_DECLTYPES)
+
+    # register numpy adapters
+    sqlite3.register_adapter(np.ndarray, Database.adaptArray)
+    sqlite3.register_converter("array", Database.convertArray)
+
+    cursor = connection.cursor()
+
+    return connection, cursor
+
+# Inputs a cursor and a name for a table
+# Outputs a list of strings with the name of every column
+# Use to extract a list of parameters for a given experiment, or to check what axes were scanned along
+def columnNames(cursor, table : str):
+
+    # Generate PRAGMA table_info query
+    query = "PRAGMA table_info(" + table + ")"
+
+    res = cursor.execute(query).fetchall()
+
+    # The result will be a list of tuples. The name of the column is entry 1
+    names = []
+    for column in res:
+        names.append(column[1])
+
+    return names
+
+# Inputs a cursor and table name
+# Outputs the number of rows in the table
+def numberOfRows(cursor, table: str):
+
+    query = "SELECT COUNT(*) FROM " + table
+
+    res = cursor.execute(query)
+
+    return res.fetchone()[0]
+
+# Convert sqlite database
+# Inputs a filename with .sqlite3 extension
+# Creates a data dict and saves it as the same filename .pickle
+# Returns the dataDict
+def sqliteToPickle(file : str):
+
+    # Open db connection
+    con, cur = openDB(file)
+
+    # get column names
+    colNames = columnNames(cur, 'data')
+
+    # find the position of collection_index, which is used to create keys for the dataDict
+    indexPosition = colNames.index('experimentNumber')
+
+    dataDict = {}
+
+    # Generate the filename by removing .sqlite3 extension and .pickle extension
+    pickleFile = os.path.splitext(file)[0] + '.pickle'
+    dataDict['fileName'] = pickleFile
+
+    # check if the pickle file already exists. If it does, print an error message and exit early
+    if os.path.isfile(pickleFile):
+        print("sqliteToPickle Warning: pickle file " + pickleFile + " already exists. Conversion aborted.")
+        return -1
+
+    # Gather the number of rows in the db
+    numRows = numberOfRows(cur, 'data')
+
+    # Create and execute a SELECT query to pull all of the data from the table
+    selectQuery = "SELECT " + ", ".join(colNames) + " FROM data"
+    res = cur.execute(selectQuery)
+
+    # Iterate through the result, convert the data to numpy arrays, apply the function, record in a list with keys
+    for i in tqdm(range(numRows)):
+
+        row = res.fetchone()
+
+        #create a new dict for the given collection_index
+        index = int(row[indexPosition])
+        dataDict[index] = {}
+
+        for i in range(len(colNames)):
+            # some tables have blank columns due to code bugs. This skips over them
+            # needs to first check if the value is an array b/c truth values don't apply to whole arrays
+            if type(row[i]) == np.ndarray or row[i] != None:
+                dataDict[index][colNames[i]] = stringConverter(row[i])
+            else:
+                pass
+
+    # save the dataDict as a pickle. We checked if the file exists earlier, so this operation is safe
+    with open(pickleFile, 'wb') as f:
+        pickle.dump(dataDict, f)
+
+    con.close()
+    f.close()
+
+    return dataDict
+
+# todo: update description - no longer just for strings!
+# Helper function that takes in a string returned from a table lookup and attempts to convert it to the appropriate data type
+# Only handles floats and lists right now. If it isn't recognized, it returns the unchanged string
+def stringConverter(string):
+
+    # first check if its an ndarray and return
+    if type(string) == np.ndarray:
+        return string
+
+    # Next attempt a float
+    try:
+        data = float(string)
+        return data
+    except ValueError or TypeError:
+        # it isn't a float, so try to convert a list
+        # lists are deprecated but kept in for backward compatibility
+        try:
+            data = stringListToArray(string)
+            return data
+        # it isn't a list either. return the string unchanged
+        except json.decoder.JSONDecodeError:
+            return string
+
+# Helper function to convert a 'stringified' list ('[1.1, 3.2, 4.3]') into a numpy array ([1.1, 3.2, 4.3])
+# Inputs the string, outputs the list
+# Used to convert sql-saved lists into numpy arrays
+# TODO: in the future, saving and loading can be made smarter - save the data as binary and directly load it into an array
+def stringListToArray(strList : str):
+
+    # For some reason it is faster to use json.loads rather than np.fromstring
+    return np.array(json.loads(strList))
+
+# Saves a dataDict as a pickle. If the 'fileName' key is not informed, a warning message is printed
+def savePickle(dataDict : dict):
+
+    if 'fileName' not in dataDict.keys():
+        print('savePickle Error: \'fileName\' is not a key in input dict, pickle cannot be saved. Manually add dataDict[\'fileName\'] = fileName and retry saving.'
+              'In the future, using loadPickle() or sqliteToPickle() ensures the dataDict is properly formatted')
+        return -1
+
+    else:
+        fileName = dataDict['fileName']
+        with open(fileName, 'wb') as f:
+            pickle.dump(dataDict, f)
+        f.close()
+        return 0
+
+# Load a pickle specified in a filename
+# This will also do some basic error checking:
+#   Makes sure the pickle is a dict
+#   Checks if the 'fileName' key exists
+#   Checks if the 'fileName' value matches the input fileName.
+#       If either of the last two are not true, the 'fileName' key is updated
+# NOTE: the first warning message will be thrown for loading DataCubes. This should be fine
+def loadPickle(fileName : str):
+
+    with open(fileName, 'rb') as f:
+        dataDict = pickle.load(f)
+
+    pickleType = type(dataDict)
+
+    if pickleType != dict:
+        print('loadPickle Warning: loading ' + fileName + ' does not result in a dict. Data manipulation functions and scripts will likely fail.')
+
+    # handle problems with dataDict fileName field
+    elif pickleType == dict and 'fileName' not in dataDict.keys():
+        print('loadPickle Warning: \'fileName\' not in list of dataDict keys. Updated dataDict[\'fileName\'] = ' + fileName)
+        dataDict['fileName'] = fileName
+        savePickle(dataDict)
+
+    elif pickleType == dict and dataDict['fileName'] != fileName:
+        print('loadPickle Warning: dataDict[\'fileName\'] does not match input fileName. Value of dataDict key has been updated to match new file location.')
+        dataDict['fileName'] = fileName
+        savePickle(dataDict)
+
+    f.close()
+    return dataDict
+
+# apply function to key
+# takes a dataDict, a function, the key to store the result in, a list of keys to use as the function arguments, and a list of additional arguments if needed
+# NOTE: if dataDict[collection_index][resKey] already exists, it will be overwritten
+def applyFunctionToData(dataDict : dict, func, resKey, dataKeys, *funcArgs):
+
+    # check if dataKeys is a list. If it isn't convert it to one
+    dataKeys = [dataKeys] if not isinstance(dataKeys, list) else dataKeys
+
+    # Iterate through the keys (coordinates) in the dataDict
+    for key in dataDict:
+
+        # check that the key is a collection_index (an int), not 'fileName' or 'parameters"
+        if type(key) == int:
+
+            # Gather the data from dataKeys into a list to use as input to func
+            funcInputs = [dataDict[key][dataKey] for dataKey in dataKeys]
+
+            dataDict[key][resKey] = func(*funcInputs, *funcArgs)
+
+        else:
+            pass
+
+    # Repickle the data
+    savePickle(dataDict)
+
+    return dataDict
